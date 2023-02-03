36710d1c67115d2da804821141ff631027213e4d
AhadTaghiyev <ahad.taghiyev@gmail.com>
2023-02-04 03:56:02 +0400

    Created Product Entity and installed mongo db

---
+++ src/MicroServicesEcommerce/Services/Catalog/WebApplication1/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string ImageFile { get; set; }
+        public double Price { get; set; }
+    }
+}
