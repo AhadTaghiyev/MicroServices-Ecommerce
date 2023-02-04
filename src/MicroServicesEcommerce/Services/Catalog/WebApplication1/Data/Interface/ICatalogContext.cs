19bbda8562f54028b8a1873116738a2efebee0cf
AhadTaghiyev <ahad.taghiyev@gmail.com>
2023-02-04 04:24:00 +0400

    created is catalogcontext

---
+++ src/MicroServicesEcommerce/Services/Catalog/WebApplication1/Data/Interface/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Driver;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Data.Interface
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
